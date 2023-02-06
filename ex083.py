ea4b98c46fca2ca9d3da1504a774c0ef20518a40
vento-leste <lucas.damasceno.dev@gmail.com>
2023-02-05 22:57:17 -0300

    Create a program that will read several numbers and put them in a list. After that, create two extra lists that will contain only the even and odd values entered, respectively. At the end, show the contents of the three generated lists.

---
+++ ex083.py
@@ -0,0 +1,15 @@
+expressao = input("Digite a expressão: ")
+pilha = []
+for char in expressao:
+    if char == '(':
+        pilha.append(char)
+    elif char == ')':
+        if not pilha:
+            print("A expressão está incorreta.")
+            break
+        pilha.pop()
+else:
+    if not pilha:
+        print("A expressão está correta.")
+    else:
+        print("A expressão está incorreta.")
