9054f636181f31c52197b65944dbf257f2101406
vento-leste <lucas.damasceno.dev@gmail.com>
2023-02-23 17:00:44 -0300

    Write a program that has a list called numbers and two functions called sort() and sumPair(). The first function will draw 5 numbers and place them inside the list and the second function will show the sum of all the even values drawn by the previous function.

---
+++ ex100.py
@@ -0,0 +1,21 @@
+from random import randint
+
+
+def sorteio():
+    numeros = []
+    for i in range(5):
+        numeros.append(randint(1, 10))
+    print(f"Sorteando 5 valores da lista: {numeros}. Pronto.")
+    return numeros
+
+
+def soma_par():
+    soma_pares = 0
+    numeros = sorteio()
+    for numero in numeros:
+        if numero % 2 == 0:
+            soma_pares += numero
+    print(f"Soma dos valores pares de {numeros}: {soma_pares}")
+
+
+soma_par()
